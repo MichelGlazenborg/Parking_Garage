4a3151267fdd62d478a62e3ce765ab4ed169fa14
Michel Glazenborg <Michel.Glazenborg@outlook.com>
2017-01-11 13:19:45 +0100

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("This is a test.");
-        System.out.println("This is a test.");
-        System.out.println("This is a test.");
     }
 }
