9c14c48d72764179f01d17b92ce49c38d7aaecfd
CrazyMazzo <w.slager123@gmail.com>
2017-01-11 14:00:21 +0100

    Hellllo

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
     public static void main(String[] args) {
         Simulator sim = new Simulator();
         sim.run();
-        System.out.println("Hello??");
-        System.out.println("Hello??");
+        System.out.println("Just some testing");
     }
 }
