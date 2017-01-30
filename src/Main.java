ef9bccc984d5eddee37d36c215cf24028376173e
Robert Monden <cj_robert@outlook.com>
2017-01-30 15:55:14 +0100

    The screen should be maximized automatically

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("View/Parkeergarage.fxml"));
+        primaryStage.setMaximized(true);
         primaryStage.setTitle("Parking simulator");
         primaryStage.setScene(new Scene(root, 1200, 800));
         primaryStage.show();
