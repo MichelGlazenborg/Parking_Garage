efc5ac95da4df296e7afe6526b989b082b9a347d
Jelmer Haarman <jelmer.internet@xs4all.nl>
2017-02-06 21:17:24 +0100

    update

--- src/models/Garage.java
+++ src/models/Garage.java
@@ -75,13 +75,13 @@ public class Garage {
         if (!locationIsValid(location)) {
             return false;
         }
-            Car oldCar = getCarAt(location);
-            if (oldCar == null) {
-                _cars[location.getFloor()][location.getRow()][location.getPlace()] = car;
-                car.setLocation(location);
-                _numberOfOpenSpots--;
-                return true;
-            }
+        Car oldCar = getCarAt(location);
+        if (oldCar == null) {
+            _cars[location.getFloor()][location.getRow()][location.getPlace()] = car;
+            car.setLocation(location);
+            _numberOfOpenSpots--;
+            return true;
+        }
 
         return false;
     }
@@ -111,8 +111,8 @@ public class Garage {
         _numberOfPassHolderSpots = numberOfSpots;
 
         int x = 0,
-            z = 0,
-            y = 0;
+                z = 0,
+                y = 0;
 
         for (int i=0; i<numberOfSpots; i++) {
             if (z == 30) {
@@ -271,23 +271,14 @@ public class Garage {
         return _numberOfPlaces;
     }
 
-<<<<<<< HEAD:src/models/Garage.java
     public int getNumberOfAdHoc() {
         return _currentAdHoc;
     }
-=======
-        public CarParkView(Canvas canvas) {
-            _canvas = canvas;
-            _graphicsContext = canvas.getGraphicsContext2D();
-            _graphicsContext.drawImage(canvasBackground, 0, 0, 560, 335);
-        }
->>>>>>> update:src/models/SimulatorView.java
 
     public int getNumberOfPassHolders() {
         return _currentPassHolders;
     }
 
-<<<<<<< HEAD:src/models/Garage.java
     public int getNumberOfCarsWithReservation() {
         return _currentCarsWithReservation;
     }
@@ -299,19 +290,6 @@ public class Garage {
     public int getNumberOfPassHolderSpots() {
         return _numberOfPassHolderSpots;
     }
-=======
-            for (int floor = 0; floor <getNumberOfFloors(); floor++) {
-                for (int row = 0; row < getNumberOfRows(); row++) {
-                    for (int place = 0; place < getNumberOfPlaces(); place++) {
-                        Location location = new Location(floor, row, place);
-                        Car car = getCarAt(location);
-                        Color color = (car == null ? Color.WHITE : car.getColor());
-                        drawParkingSpot(location, color);
-                    }
-                }
-            }
-        }
->>>>>>> update:src/models/SimulatorView.java
 
     public int getPassHolderSpots() {
         return _numberOfPassHolderSpots;
