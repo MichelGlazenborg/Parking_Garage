424da30c9cfd4c9f0e33fa7aebbc4eed5b1552cc
Robert Monden <cj_robert@outlook.com>
2017-02-06 12:13:05 +0100

    Trying to get the gridpane to display correctly

--- src/controller/TimeInputPopupController.java
+++ src/controller/TimeInputPopupController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import javafx.scene.Scene;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import models.Popup;
 import models.TimeInputPopup;
@@ -13,23 +14,41 @@ public class TimeInputPopupController extends Popup {
     private TimeInputPopup _model;
     private TimeInputPopupView _view;
 
+    private static final int SCENE_WIDTH = 400;
+    private static final int SCENE_HEIGHT = 300;
+
     public TimeInputPopupController() {
         init(true, true);
 
-        _layout = new GridPane();
+        setupLayout(); _layout = new GridPane();
         _view = new TimeInputPopupView(_layout);
 
         _model = new TimeInputPopup(_view);
         _model.setLayout(_layout);
 
+        setup();
+
+        _scene = new Scene(_layout, SCENE_WIDTH, SCENE_HEIGHT);
+
         setTitle("Please enter the date and time");
+        setScene(_scene);
     }
 
     public void show() {
-        _model.setFields();
-        _scene = new Scene(_layout);
-        setScene(_scene);
-        sizeToScene();
         super.show();
+    }
+
+    private void setupLayout() {
+        _layout = new GridPane();
+        ColumnConstraints colLabel = new ColumnConstraints();
+        ColumnConstraints colFields = new ColumnConstraints();
+
+        colLabel.setPercentWidth(50);
+        colFields.setPercentWidth(50);
+        _layout.getColumnConstraints().addAll(colLabel, colFields);
+    }
+
+    private void setup() {
+        _model.setFields();
     }
 }
--- src/view/TimeInputPopupView.java
+++ src/view/TimeInputPopupView.java
@@ -12,6 +12,9 @@ public class TimeInputPopupView {
     private static final int ROW_FIELD_HOUR = 6;
     private static final int ROW_FIELD_MINUTE = 8;
 
+    private static final int COLUMN_LABELS = 1;
+    private static final int COLUMN_FIELDS = 2;
+
     public TimeInputPopupView(GridPane layout) {
         _layout = layout;
         _layout.setVgap(10);
@@ -19,17 +22,19 @@ public class TimeInputPopupView {
     }
 
     public void addLabels(Label week, Label day, Label hour, Label minute) {
-        GridPane.setConstraints(week, 1, ROW_FIELD_WEEK);
-        GridPane.setConstraints(day, 1, ROW_FIELD_DAY);
-        GridPane.setConstraints(hour, 1, ROW_FIELD_HOUR);
-        GridPane.setConstraints(minute, 1, ROW_FIELD_MINUTE);
+        GridPane.setConstraints(week, COLUMN_LABELS, ROW_FIELD_WEEK);
+        GridPane.setConstraints(day, COLUMN_LABELS, ROW_FIELD_DAY);
+        GridPane.setConstraints(hour, COLUMN_LABELS, ROW_FIELD_HOUR);
+        GridPane.setConstraints(minute, COLUMN_LABELS, ROW_FIELD_MINUTE);
+
+        _layout.getChildren().addAll(week, day, hour, minute);
     }
 
     public void addFields(TextField setWeek, TextField setDay, TextField setHour, TextField setMinute) {
-        GridPane.setConstraints(setWeek, 3, ROW_FIELD_WEEK);
-        GridPane.setConstraints(setDay, 3, ROW_FIELD_DAY);
-        GridPane.setConstraints(setHour, 3, ROW_FIELD_HOUR);
-        GridPane.setConstraints(setMinute, 3, ROW_FIELD_MINUTE);
+        GridPane.setConstraints(setWeek, COLUMN_FIELDS, ROW_FIELD_WEEK);
+        GridPane.setConstraints(setDay, COLUMN_FIELDS, ROW_FIELD_DAY);
+        GridPane.setConstraints(setHour, COLUMN_FIELDS, ROW_FIELD_HOUR);
+        GridPane.setConstraints(setMinute, COLUMN_FIELDS, ROW_FIELD_MINUTE);
 
         _layout.getChildren().addAll(setWeek, setDay, setHour, setMinute);
     }
