410fcbb70226ca30943281cbd4550cb115094ab7
Jelmer Haarman <jelmer.internet@xs4all.nl>
2017-02-07 13:12:24 +0100

    popup heigth verhoogt

--- src/models/Simulator.java
+++ src/models/Simulator.java
@@ -287,7 +287,7 @@ public class Simulator {
                     _garage.addOneCarToCount("ParkingPassCar");
                 }
             }
-			i += 0.33;
+			i += 1;
 
     	    switch (getTime()[2]) {
                 case 0:
