71f310f9483f37cb88d54b086528dcd336cd9cb3
Robert Monden <cj_robert@outlook.com>
2017-02-08 12:29:39 +0100

    Added documentation

--- src/models/Car.java
+++ src/models/Car.java
@@ -2,16 +2,47 @@ package models;
 
 import javafx.scene.paint.Color;
 
+/**
+ * This abstract class is used by several car type classes. It contains several
+ * methods that can be used by these car type classes.
+ * @author  MATA
+ * @since   1.0
+ */
 public abstract class Car {
 
+    /**
+     * This property refers to the location of the car.
+     */
     private Location _location;
 
+    /**
+     * How many minutes is the car supposed to stay parked in our beloved garage?
+     */
     private int _stayMinutes;
+
+    /**
+     * How much time (in minutes) remains until the car leaves.
+     */
     private double _minutesLeft;
 
+    /**
+     * Is the car currently paying?
+     */
     private boolean _isPaying;
+
+    /**
+     * Determines if the car has to pay
+     */
     private boolean _hasToPay;
+
+    /**
+     * If the car has a reservation, this should be set to true.
+     */
     private boolean _hasReservation;
+
+    /**
+     * If the car is currently in the entrance queue, this should be set to true.
+     */
     private boolean _readyToEnter;
 
     /**
@@ -23,31 +54,39 @@ public abstract class Car {
 
     /**
      * Returns the location of a car
-     * @return Location location of the car
+     * @return  Location    Location of the car
      */
     public Location getLocation() {
         return _location;
     }
 
     /**
-     * Sets the location of a car
-     * @param location the location of a car
+     * Sets the location of a car.
+     * @param   location    The location of a car
      */
     public void setLocation(Location location) {
         _location = location;
     }
 
+    /**
+     * Used for determining whether or not the car is ready to enter.
+     * @return  boolean
+     */
     public boolean returnReadyToEnter() {
         return _readyToEnter;
     }
 
-    public void set_readyToEnter(boolean b) {
-        _readyToEnter = b;
+    /**
+     * Sets the value of _readyToEnter
+     * @param   isReady   Should be true if the car is ready to enter, false if it's not
+     */
+    public void set_readyToEnter(boolean isReady) {
+        _readyToEnter = isReady;
     }
 
     /**
      * Returns the number of minutes left of parking
-     * @return int time left in minutes
+     * @return  time    Left in minutes
      */
     public double getMinutesLeft() {
         return _minutesLeft;
@@ -55,7 +94,7 @@ public abstract class Car {
 
     /**
      * Sets the number of minutes a car has left
-     * @param minutesLeft time left in minutes
+     * @param   minutesLeft   Time left in minutes
      */
     public void setMinutesLeft(double minutesLeft) {
         _minutesLeft = minutesLeft;
@@ -63,7 +102,7 @@ public abstract class Car {
 
     /**
      * Returns if the car is paying
-     * @return boolean car is paying
+     * @return  boolean     True/false depending on whether the car is paying or not
      */
     public boolean getIsPaying() {
         return _isPaying;
@@ -71,16 +110,24 @@ public abstract class Car {
 
     /**
      * Sets if the car is paying
-     * @param isPaying is the car paying
+     * @param   isPaying    Is the car paying
      */
     public void setIsPaying(boolean isPaying) {
         _isPaying = isPaying;
     }
 
+    /**
+     * Returns the value of _stayMinutes
+     * @return  int     The time in minutes the car has left
+     */
     public int getStayMinutes(){
         return _stayMinutes;
     }
 
+    /**
+     * Sets the value of _stayMinutes
+     * @param   time    The amount of time (in minutes) the car gets to stay.
+     */
     public void setStayMinutes(int time) {
         _stayMinutes = time;
     }
@@ -93,25 +140,39 @@ public abstract class Car {
         return _hasToPay;
     }
 
+    /**
+     * Gets the value of _hasReservation
+     * @return  boolean     The value of _hasReservation
+     */
     public boolean getHasReservation() {
         return  _hasReservation;
     }
 
+    /**
+     * Sets the value of _hasReservation
+     * @param   hasReservation  True/false depending on whether the car's owner has a reservation
+     */
     public void setHasReservation(boolean hasReservation) {
         _hasReservation = hasReservation;
     }
 
     /**
      * Sets if the car has to pay
-     * @param hasToPay does the car need to pay
+     * @param   hasToPay    Does the car need to pay
      */
     public void setHasToPay(boolean hasToPay) {
         _hasToPay = hasToPay;
     }
 
+    /**
+     * Reduces the minutes left by one.
+     */
     public void tick() {
         _minutesLeft--;
     }
-    
+
+    /**
+     * All car classes that extend this class should definitely have this method.
+     */
     public abstract Color getColor();
 }
--- src/models/Garage.java
+++ src/models/Garage.java
@@ -9,23 +9,55 @@ import javafx.scene.media.MediaPlayer;
 
 import view.GarageView;
 
+/**
+ * This class is used for the garage itself
+ * @author  MATA
+ * @since   1.0
+ */
 public class Garage {
 
+    /**
+     * Numbers of floors, rows, places and open spots. There's also a property
+     * for the amount of pass holder spots, referring to the amount of spots
+     * reserved for pass holders' cars.
+     */
     private int _numberOfFloors;
     private int _numberOfRows;
     private int _numberOfPlaces;
     private int _numberOfOpenSpots;
     private int _numberOfPassHolderSpots;
+
+    /**
+     * Two simulator settings; the speed of the simulation and whether or not
+     * the program should play sounds.
+     */
     private double _speed;
     private boolean _playSound = true;
+
+    /**
+     * This array is used for the car spots.
+     */
     private Car[][][] _cars;
 
+    /**
+     * An object of the GarageView class. This is used as the view.
+     */
     private GarageView _garageView;
 
+    /**
+     * The number of pass holders, ad hoc cars and cars whose owners have a reservation.
+     */
     private int _currentPassHolders;
     private int _currentAdHoc;
     private int _currentCarsWithReservation;
 
+    /**
+     * Creates a new garage
+     * @param   canvas              The main canvas. This will be used for drawing on.
+     * @param   numberOfFloors      The number of floors
+     * @param   numberOfRows        The number of rows
+     * @param   numberOfPlaces      The number of places
+     */
     public Garage(Canvas canvas, int numberOfFloors, int numberOfRows, int numberOfPlaces) {
         _numberOfFloors = numberOfFloors;
         _numberOfRows = numberOfRows;
@@ -40,22 +72,40 @@ public class Garage {
         _currentCarsWithReservation = 0;
     }
 
+    /**
+     * Sets the simulation speed
+     * @param   speed   How fast the simulator should run
+     */
     public void setSpeed(double speed) {
         _speed = speed;
     }
 
+    /**
+     * @return  boolean     Whether or not the program will play sounds.
+     */
     public boolean getPlaySound() {
         return _playSound;
     }
 
+    /**
+     * Sets the value of _playSound
+     * @param   a   Whether or not the program should play sounds
+     */
     public void setPlaySound(boolean a) {
         _playSound = a;
     }
 
+    /**
+     * Updates the view
+     */
     public void updateView() {
         _garageView.update();
     }
 
+    /**
+     * Removes one car from the respective car count
+     * @param   type    The name of the class
+     */
     public void removeCarFromCount(String type) {
         switch (type) {
             case "AdHocCar":
@@ -70,6 +120,10 @@ public class Garage {
         }
     }
 
+    /**
+     * Adds one car to the respective car count
+     * @param   type    The name of the class
+     */
     public void addOneCarToCount(String type) {
         switch (type) {
             case "AdHocCar":
@@ -84,6 +138,11 @@ public class Garage {
         }
     }
 
+    /**
+     * Returns a car object
+     * @param   location    The car's location
+     * @return  car         The car currently parked at the given location
+     */
     public Car getCarAt(Location location) {
         if (!locationIsValid(location))
             return null;
@@ -91,10 +150,16 @@ public class Garage {
         return _cars[location.getFloor()][location.getRow()][location.getPlace()];
     }
 
+    /**
+     * Erases the canvas
+     */
     public void eraseCanvas() {
         _garageView.eraseCanvas();
     }
 
+    /**
+     * Plays the sound
+     */
     public void playSound() {
         String musicFile = "src/assets/ping.mp3";
 
@@ -109,11 +174,18 @@ public class Garage {
         }
     }
 
+    /**
+     * Adds a new car to the given location
+     * @param   location        The location
+     * @param   car             The car that should be added
+     * @return  boolean         Whether or not the car could be added
+     */
     public boolean setCarAt(Location location, Car car) {
         if (!locationIsValid(location)) {
             return false;
         }
 
+        // Can't park a car if there's already a car in the given spot
         Car oldCar = getCarAt(location);
         if (oldCar == null) {
             _cars[location.getFloor()][location.getRow()][location.getPlace()] = car;
@@ -129,7 +201,11 @@ public class Garage {
         return false;
     }
 
+    /**
+     * Resets the simulation.
+     */
     public void reset() {
+        // Reset the cars
         for (int i = 0; i < getNumberOfFloors(); i++) {
             for (int j = 0; j < getNumberOfRows(); j++) {
                 for (int k = 0; k < getNumberOfPlaces(); k++) {
@@ -140,6 +216,7 @@ public class Garage {
             }
         }
 
+        // Reset the counters
         _numberOfPassHolderSpots = 0;
         _currentAdHoc = 0;
         _currentPassHolders = 0;
@@ -148,7 +225,7 @@ public class Garage {
 
     /**
      * Makes passHolder spots
-     * @param numberOfSpots the number of passHolder spots you want
+     * @param   numberOfSpots   The number of passHolder spots you want
      */
     public void makePassHolderSpots(int numberOfSpots) {
         _numberOfPassHolderSpots = numberOfSpots;
@@ -175,6 +252,12 @@ public class Garage {
         updateView();
     }
 
+    /**
+     * Creates a new reservation
+     * @param   loc         The location
+     * @param   minute      The minute the reservation was made
+     * @param   hour        The hour the reservation was made
+     */
     public void makeReservationsAt(Location loc , int minute, int hour) {
         Reservation res = new Reservation(minute, hour);
         setReservation(loc, res);
@@ -183,6 +266,12 @@ public class Garage {
         updateView();
     }
 
+    /**
+     * Sets a pass holder space
+     * @param   loc         The location
+     * @param   phs         The pass holder space
+     * @return  boolean     Whether or not the action was successful
+     */
     public boolean setPassHolderSpace(Location loc, PassHolderSpace phs) {
         if (!locationIsValid(loc))
             return false;
@@ -197,6 +286,12 @@ public class Garage {
         return false;
     }
 
+    /**
+     * Sets a new reservation
+     * @param   loc     The car's location
+     * @param   res     An object of the reservation class
+     * @return  boolean Whether or not the action was succesful
+     */
     public boolean setReservation(Location loc, Reservation res) {
         if (!locationIsValid(loc))
             return false;
@@ -210,6 +305,11 @@ public class Garage {
         return false;
     }
 
+    /**
+     * Removes a car from the specified location
+     * @param   location    The location
+     * @return  car         The car that was removed
+     */
     public Car removeCarAt(Location location) {
         if (!locationIsValid(location))
             return null;
@@ -224,6 +324,10 @@ public class Garage {
         return car;
     }
 
+    /**
+     * Gets the first free location.
+     * @return  location    The parking spot that is currently unoccupied.
+     */
     public Location getFirstFreeLocation() {
         for (int floor = 0; floor < getNumberOfFloors(); floor++) {
             for (int row = 0; row < getNumberOfRows(); row++) {
@@ -237,6 +341,11 @@ public class Garage {
         return null;
     }
 
+    /**
+     * Gets the first free reservation.
+     * @param   cTime       When is the car supposed to enter?
+     * @return  location    The location of a reservation
+     */
     public Location getFirstReservation(int[] cTime) {
         for (int floor = 0; floor < getNumberOfFloors(); floor++) {
             for (int row = 0; row < getNumberOfRows(); row++) {
@@ -257,6 +366,10 @@ public class Garage {
         return null;
     }
 
+    /**
+     * Gets the first free parking pass spot.
+     * @return  location    The location
+     */
     public Location getFirstPassSpot() {
         for (int floor = 0; floor < getNumberOfFloors(); floor++) {
             for (int row = 0; row < getNumberOfRows(); row++) {
@@ -275,6 +388,10 @@ public class Garage {
     }
 
 
+    /**
+     * Gets the first leaving car
+     * @return  car     The car that is about to leave.
+     */
     public Car getFirstLeavingCar() {
         for (int floor = 0; floor < getNumberOfFloors(); floor++) {
             for (int row = 0; row < getNumberOfRows(); row++) {
@@ -289,6 +406,9 @@ public class Garage {
         return null;
     }
 
+    /**
+     * Runs one tick (i.e. one second)
+     */
     public void tick() {
         for (int floor = 0; floor < getNumberOfFloors(); floor++) {
             for (int row = 0; row < getNumberOfRows(); row++) {
@@ -302,6 +422,11 @@ public class Garage {
         }
     }
 
+    /**
+     * Determines whether or not the location is valid
+     * @param   location    The location
+     * @return  boolean     Whether or not the location is valid
+     */
     private boolean locationIsValid(Location location) {
         int floor = location.getFloor();
         int row = location.getRow();
@@ -309,44 +434,89 @@ public class Garage {
         return !(floor < 0 || floor >= _numberOfFloors || row < 0 || row > _numberOfRows || place < 0 || place > _numberOfPlaces);
     }
 
+    /**
+     * @return  int     The number of floors
+     */
     public int getNumberOfFloors() {
         return _numberOfFloors;
     }
 
-    public void set_numberOfFloors(int floors) {_numberOfFloors = floors;}
+    /**
+     * Sets the number of floors
+     * @param   floors  The number of floors
+     */
+    public void set_numberOfFloors(int floors) {
+        _numberOfFloors = floors;
+    }
 
+    /**
+     * @return  int     The number of rows
+     */
     public int getNumberOfRows() {
         return _numberOfRows;
     }
 
-    public void set_numberOfRows(int rows) {_numberOfRows = rows;}
+    /**
+     * Sets the number of rows
+     * @param   rows    The number of rows
+     */
+    public void set_numberOfRows(int rows) {
+        _numberOfRows = rows;
+    }
 
+    /**
+     * @return  int     The number of parking spots per row.
+     */
     public int getNumberOfPlaces() {
         return _numberOfPlaces;
     }
 
-    public void set_numberOfPlaces(int places) { _numberOfPlaces = places; }
+    /**
+     * Sets the number of parking spots per row.
+     * @param places
+     */
+    public void set_numberOfPlaces(int places) {
+        _numberOfPlaces = places;
+    }
 
+    /**
+     * @return  int     The number of ad hoc cars currently in the garage
+     */
     public int getNumberOfAdHoc() {
         return _currentAdHoc;
     }
 
+    /**
+     * @return  int     The number of pass holder cars currently in the garage
+     */
     public int getNumberOfPassHolders() {
         return _currentPassHolders;
     }
 
+    /**
+     * @return  int     The number of cars with a reservation that are currently in the garage.
+     */
     public int getNumberOfCarsWithReservation() {
         return _currentCarsWithReservation;
     }
 
+    /**
+     * @return  int     The number of open spots
+     */
     public int getNumberOfOpenSpots() {
         return _numberOfOpenSpots;
     }
 
+    /**
+     * @return  int     The number of pass holder spots
+     */
     public int getNumberOfPassHolderSpots() {
         return _numberOfPassHolderSpots;
     }
 
+    /**
+     * @return  int     The number of pass holder spots
+     */
     public int getPassHolderSpots() {
         return _numberOfPassHolderSpots;
     }
